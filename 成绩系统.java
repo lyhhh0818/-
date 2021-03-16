0a9dc63d4bbcd1234f5c7c84d33f804d4b52fcb7
lyhhh0818 <71809307+lyhhh0818@users.noreply.github.com>
2021-03-16 16:59:45 +0800

    Add files via upload

---
+++ 成绩系统.java
@@ -0,0 +1,103 @@
+package 作业;
+
+import java.util.Scanner;
+
+public class 成绩系统 {
+	public static int score[];
+	public static void main(String[] args) {
+		
+		int choose=menu();
+		while(choose!=5) {
+			switch(choose) {
+			case 1:  
+				enterResults()  ;break;
+			case 2:  
+				if(score==null) {
+					System.out.println("没有输入成绩，请选择1");
+					break;
+				}
+				else
+				classAverage(score)  ;break;
+			case 3: 
+				if(score==null) {
+					System.out.println("没有输入成绩，请选择1");
+					break;
+				}
+				else
+				highestGrade(score)  ;break;
+			case 4:
+				if(score==null) {
+					System.out.println("没有输入成绩，请选择1");
+					break;
+				}
+				else
+				lowestGrade (score)  ;break;
+			default:   System.out.println("输入错误，重新输入");		
+			}
+			choose=menu();
+		}
+		System.out.println("再见");
+	}
+
+	public static int menu() {
+		int choice = 0;
+		System.out.println("**********************");
+		System.out.println("1.enter results");
+		System.out.println("2.class average");
+		System.out.println("3.highest grade");
+		System.out.println("4.lowest grade");
+		System.out.println("5.exit");
+		System.out.println("请输入（1~5）");		
+		Scanner scan=new Scanner(System.in);		
+		choice=scan.nextInt();
+		return choice;
+	}
+
+	public static void enterResults(){	
+		score=new int [5];
+		int flag=1;
+		for(int i=0; i<score.length;i++) {
+			System.out.println("请输入第"+(i+1)+"个同学成绩");
+			flag=1;
+			while(flag==1) {
+				Scanner scan=new Scanner(System.in);
+				score[i]=scan.nextInt();
+				if(score[i]<=0||score[i]>100)  System.out.println("输入错误");
+				else flag=0;
+			}
+			
+		}
+		
+	}
+
+	public static void classAverage(int score[]) {
+		
+		double sum=0,avg;
+		for(int i=0; i<score.length;i++) {
+			sum=sum+score[i];
+		}
+		avg=sum/score.length;
+		System.out.println("平均成绩为"+avg);
+		
+	}
+
+	public static void highestGrade(int score[]) {
+		int num;
+		num=score[0];
+		for(int i=0; i<score.length;i++) {
+			if(score[i]>num) num=score[i];
+		}
+		System.out.println("最大值为"+num);
+	}
+
+	public static void lowestGrade(int score[]) {
+		int num;
+		num=score[0];
+		for(int i=0; i<score.length;i++) {
+			if(score[i]<num) num=score[i];
+		}
+		System.out.println("最小值为"+num);
+		
+	}
+
+}
